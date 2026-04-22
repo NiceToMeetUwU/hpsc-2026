41611647d20a4ecbb927d99f4cb4d8a223bdbc50
Nice2meetUwU <andyyang122333@gmail.com>
2026-04-22 18:22:48 +0900

    changed pi.cpp

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,12 +1,21 @@
 #include <cstdio>
+#include <omp.h>
 
 int main() {
-  int n = 10;
-  double dx = 1. / n;
-  double pi = 0;
-  for (int i=0; i<n; i++) {
-    double x = (i + 0.5) * dx;
-    pi += 4.0 / (1.0 + x * x) * dx;
-  }
-  printf("%17.15f\n",pi);
+    int n = 100000; // test bigger number?
+    double dx = 1. / n;
+    double pi = 0;
+    
+    double start_time = omp_get_wtime(); // start timing
+
+    #pragma omp parallel for reduction(+:pi)
+    for (int i=0; i<n; i++) {
+        double x = (i + 0.5) * dx;
+        pi += 4.0 / (1.0 + x * x) * dx;
+    }
+
+    double end_time = omp_get_wtime();
+
+    printf("Pi: %17.15f\n", pi);
+    printf("Time: %f seconds\n", end_time - start_time);
 }
