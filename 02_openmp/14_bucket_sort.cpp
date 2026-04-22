2155292f805b5c093cabf44a92360af025b5e9df
Nice2meetUwU <andyyang122333@gmail.com>
2026-04-22 20:22:49 +0900

    bucket

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,32 +1,50 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
 
 int main() {
-  int n = 50;
-  int range = 5;
-  std::vector<int> key(n);
-  for (int i=0; i<n; i++) {
-    key[i] = rand() % range;
-    printf("%d ",key[i]);
-  }
-  printf("\n");
-
-  std::vector<int> bucket(range,0); 
-  for (int i=0; i<n; i++)
-    bucket[key[i]]++;
-  std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
-  for (int i=0; i<range; i++) {
-    int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
-      key[j++] = i;
+    int n = 500; 
+    int range = 10;
+    std::vector<int> key(n);
+    
+    // 1. initialize
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        key[i] = rand() % range;
+        printf("%d ",key[i]);
     }
-  }
+    printf("\n");
+    std::vector<int> bucket(range, 0);
 
-  for (int i=0; i<n; i++) {
-    printf("%d ",key[i]);
-  }
-  printf("\n");
+    // 2. frequency
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        #pragma omp atomic
+        bucket[key[i]]++;
+    }
+
+    // 3. offset
+    std::vector<int> offset(range, 0);
+    for (int i = 1; i < range; i++) {
+        offset[i] = offset[i - 1] + bucket[i - 1];
+    }
+
+    // 4.bucket time
+    std::vector<int> bucket_count = bucket; 
+    
+    #pragma omp parallel for
+    for (int i = 0; i < range; i++) {
+        int start_pos = offset[i];
+        int count = bucket_count[i];
+        for (int j = 0; j < count; j++) {
+            key[start_pos + j] = i;
+        }
+    }
+
+    
+    for (int i = 0; i < n; i++) printf("%d ", key[i]);
+    printf(".sorted\n");
+
+    return 0;
 }
